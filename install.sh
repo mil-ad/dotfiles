839c16ae9f2836ada928f4fd22312685397eead4
Milad Alizadeh <milad2k+git@gmail.com>
2014-12-14 10:44:30 +0000

    Adding a template installer for dotbot

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
 
+set -e
 
-#TODO: Install homebrew, oh-myy-zsh, fonts and colors for OS X Terminal
+CONFIG="install.conf.yaml"
+DOTBOT_DIR="dotbot"
+
+DOTBOT_BIN="bin/dotbot"
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "${BASEDIR}"
+git submodule update --init --recursive "${DOTBOT_DIR}"
+
+"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
