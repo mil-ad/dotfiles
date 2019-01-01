0c826ec1969ac29ecd6deed99a48b44f4c215083
Milad Alizadeh <milad2k+git@gmail.com>
2019-01-01 12:20:04 +0000

    New: adding a script to set default apps. WIP

---
+++ macOS/set_default_apps.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# To find app id: osascript -e 'id of app "Name of App"'
+
+duti -v -s com.sublimetext.3 taskpaper editor
+duti -v -s com.sublimetext.3 py editor
+duti -v -s com.sublimetext.3 sh editor
+duti -v -s com.sublimetext.3 tex editor
