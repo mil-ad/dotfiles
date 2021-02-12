1a1d982c07d50b2041a7a147f08d0884744439c8
Milad Alizadeh <hi@mil.ad>
2021-02-12 23:31:26 +0000

    Removed reload from polybar launcher
    restarting i3 now restarts polybar

--- linux/.config/polybar/launch.sh
+++ linux/.config/polybar/launch.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar --reload milad &
+polybar milad &
