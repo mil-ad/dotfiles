3c2c537b835bcea8f67890377cc76c43f7de1327
Milad Alizadeh <git@mil.ad>
2022-06-21 22:29:46 +0100

    firefox polish

--- common/.mozilla/firefox/milad/user.js
+++ common/.mozilla/firefox/milad/user.js
@@ -2,3 +2,5 @@ user_pref("browser.fullscreen.autohide", false);
 user_pref("full-screen-api.warning.timeout",-1);
 user_pref("full-screen-api.transition-duration.leave", "0 0");
 user_pref("full-screen-api.transition-duration.enter", "0 0");
+
+user_pref("browser.urlbar.maxRichResults", 5);
