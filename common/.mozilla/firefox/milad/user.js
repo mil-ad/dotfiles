d9a9d3b45d03608d0b10b884cbd5cc499dca21de
Milad Alizadeh <git@mil.ad>
2022-06-21 15:33:46 +0100

    🐣 added firefox's user.js

---
+++ common/.mozilla/firefox/milad/user.js
@@ -0,0 +1,4 @@
+user_pref("browser.fullscreen.autohide", false);
+user_pref("full-screen-api.warning.timeout",-1);
+user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("full-screen-api.transition-duration.enter", "0 0");
