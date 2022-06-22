919542ed170802848d9a984a09f23b3368d5ed1a
Milad Alizadeh <git@mil.ad>
2022-06-22 09:39:26 +0100

    enabled legacy css in ff

--- common/.mozilla/firefox/milad/user.js
+++ common/.mozilla/firefox/milad/user.js
@@ -1,3 +1,5 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true)
+
 user_pref("browser.fullscreen.autohide", false);
 user_pref("full-screen-api.warning.timeout",-1);
 user_pref("full-screen-api.transition-duration.leave", "0 0");
