d6e3a772ff6aa1d3e66c30871d42c5f282dad861
Milad Alizadeh <git@mil.ad>
2022-06-22 11:34:25 +0100

    more ff

--- common/.mozilla/firefox/milad/user.js
+++ common/.mozilla/firefox/milad/user.js
@@ -1,8 +1,7 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true)
-
+user_pref("browser.urlbar.maxRichResults", 5);
 user_pref("browser.fullscreen.autohide", false);
 user_pref("full-screen-api.warning.timeout",-1);
 user_pref("full-screen-api.transition-duration.leave", "0 0");
 user_pref("full-screen-api.transition-duration.enter", "0 0");
-
-user_pref("browser.urlbar.maxRichResults", 5);
+user_pref("services.sync.prefs.sync.browser.uiCustomization.state", true);
