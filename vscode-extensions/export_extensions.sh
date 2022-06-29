ba1f4df23ac5890d4631ed630dcc92d54c09f0dc
Milad Alizadeh <git@mil.ad>
2022-06-29 12:10:41 +0100

    getting rid of vscode extensions. Letting vscode sync deal with it

--- vscode-extensions/export_extensions.sh
+++
@@ -1,8 +0,0 @@
-# !/usr/bin/env bash
-
-code --list-extensions | tee extensions-list.txt
-
-echo 
-echo ===============================
-echo Exported to extensions-list.txt
-echo ===============================
