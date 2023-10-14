23d3424db3a42eb94e10907952988e0f83a741d1
Milad Alizadeh <git@mil.ad>
2023-10-14 18:13:29 +0100

    add number aliases

--- aliases.zsh
+++ aliases.zsh
@@ -7,6 +7,17 @@ alias .....="cd ../../../.."
 alias .....="cd ../../../../.."
 alias ......="cd ../../../../../.."
 
+alias -- -='cd -'
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
 alias ssh="TERM=xterm-256color ssh"
 alias gcloud="TERM=xterm-256color gcloud"
 
