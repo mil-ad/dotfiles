476980e6bf76706fe84cccae85e16c8622db2a88
Milad Alizadeh <git@mil.ad>
2024-09-25 13:39:38 +0100

    don't include images in mpv auto playlist

--- common/.config/mpv/scripts/autoload.lua
+++ common/.config/mpv/scripts/autoload.lua
@@ -41,7 +41,7 @@ local utils = require 'mp.utils'
 
 local o = {
     disabled = false,
-    images = true,
+    images = false,
     videos = true,
     audio = true,
     additional_image_exts = "",
