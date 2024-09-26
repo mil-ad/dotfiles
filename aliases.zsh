3c12792af10c208d9e3f9891a28cc92790ba2b33
Milad Alizadeh <git@mil.ad>
2024-09-26 15:07:02 +0100

    lazygit integration, better telescope handling of hidden files

--- aliases.zsh
+++ aliases.zsh
@@ -66,7 +66,7 @@ alias lsym='ls -l `find . -maxdepth 1 -type l -print`'
 
 alias more="less"
 alias vi="nvim"
-alias vim="nvim"
+alias vim='NVIM_APPNAME="nvim-kickstart" nvim'
 alias cal='cal -3'
 
 alias df='df -h'
--- common/.config/nvim-kickstart/init.lua
+++ common/.config/nvim-kickstart/init.lua
@@ -292,7 +292,11 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        -- pickers = {}
+        pickers = {
+          find_files = {
+            find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -841,6 +845,25 @@ require('lazy').setup({
     config = function()
       require('nvim-tree').setup {}
     end,
+  },
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
   },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
