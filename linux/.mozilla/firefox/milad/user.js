e943927e2bd1d3f9d1fdc9d30b085c516312247b
Milad Alizadeh <git@mil.ad>
2024-10-20 20:22:25 +0100

    latest configs

--- linux/.mozilla/firefox/milad/user.js
+++ linux/.mozilla/firefox/milad/user.js
@@ -1,6 +1,4 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.urlbar.maxRichResults", 5);
-user_pref("browser.fullscreen.autohide", false);
 user_pref("full-screen-api.warning.timeout",-1);
 user_pref("full-screen-api.transition-duration.leave", "0 0");
 user_pref("full-screen-api.transition-duration.enter", "0 0");
@@ -8,3 +6,11 @@ user_pref("services.sync.prefs.sync.browser.uiCustomization.state", true);
 user_pref("ui.popup.disable_autohide", false);
 user_pref("privacy.webrtc.hideGlobalIndicator", true);
 user_pref("privacy.webrtc.legacyGlobalIndicator", false);
+user_pref("newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.urlbar.maxRichResults", 5);
+user_pref("browser.fullscreen.autohide", false);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.startup.homepage", "chrome://browser/content/blanktab.html");
