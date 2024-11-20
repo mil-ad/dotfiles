4b3598c4526125bdb2647d4759b9f69e5fe10f4e
Milad Alizadeh <git@mil.ad>
2024-11-20 17:07:31 +0000

    better forward/backward keybindings

--- common/.config/nvim-kickstart/init.lua
+++ common/.config/nvim-kickstart/init.lua
@@ -130,6 +130,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<C-]>', '<C-i>', { noremap = true, silent = true, desc = 'Go Forward' })
+vim.keymap.set('n', '<C-[>', '<C-o>', { noremap = true, silent = true, desc = 'Go Backward' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
