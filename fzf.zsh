2f223b7ee9eac37941e9b00b0258710b390c7534
Milad Alizadeh <git@mil.ad>
2025-01-30 09:31:54 +0000

    tweak fzf colors

--- fzf.zsh
+++ fzf.zsh
@@ -5,6 +5,7 @@ source <(fzf --zsh)
 #export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git --ignore .cache -g ""'
 export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude .git --exclude .cache .'
 export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
+export FZF_DEFAULT_OPTS='--color=pointer:#d79921 --prompt="⚡"'
 
 _fzf_compgen_path() {
   fd --hidden --follow --exclude ".git" . "$1"
