946ad50b1a415025ccae9f89d778f6240d77e4ff
Milad Alizadeh <git@mil.ad>
2025-11-22 23:14:28 +0000

    latest firefox

--- linux/.mozilla/firefox/milad/user.js
+++ linux/.mozilla/firefox/milad/user.js
@@ -8,6 +8,13 @@ user_pref("privacy.webrtc.hideGlobalIndicator", true);
 user_pref("privacy.webrtc.legacyGlobalIndicator", false);
 user_pref("newtabpage.activity-stream.feeds.topsites", false);
 user_pref("browser.urlbar.maxRichResults", 5);
+user_pref("browser.urlbar.suggest.quickactions", false)
+user_pref("browser.urlbar.suggest.openpage", false)
+user_pref("browser.urlbar.suggest.topsites", false)
+user_pref("browser.urlbar.suggest.trending", false)
+user_pref("browser.urlbar.suggest.weather", false)
+user_pref("browser.urlbar.suggest.wikipedia", false)
+user_pref("browser.urlbar.suggest.yelp", false)
 user_pref("browser.fullscreen.autohide", false);
 user_pref("browser.aboutConfig.showWarning", false);
 user_pref("browser.toolbars.bookmarks.visibility", "never");
@@ -16,10 +23,12 @@ user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
 user_pref("browser.startup.homepage", "chrome://browser/content/blanktab.html");
 user_pref("browser.fixup.domainwhitelist.go", true);
 user_pref("browser.tabs.hoverPreview.enabled", false);
+user_pref("browser.translations.neverTranslateLanguages", "fa");
 user_pref("extensions.formautofill.addresses.enabled", false);
 user_pref("extensions.formautofill.creditCards.enabled", false);
-user_pref("browser.translations.neverTranslateLanguages", "fa");
 user_pref("sidebar.animation.enabled", false)
+user_pref("sidebar.verticalTabs", true)
+user_pref("sidebar.animation.expand-on-hover.duration-ms", 0)
+user_pref("sidebar.animation.expand-on-hover.delay-duration-ms", 0)
 user_pref("signon.rememberSignons", false)
-// user_pref("sidebar.verticalTabs", true)
 user_pref("zen.workspaces.show-workspace-indicator", false)
