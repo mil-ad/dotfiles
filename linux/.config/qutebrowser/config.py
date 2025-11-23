1b9b4f94c38319736b317582c43e9f361bdb0ef2
Milad Alizadeh <git@mil.ad>
2025-11-23 10:26:02 +0000

    started using config.py

--- linux/.config/qutebrowser/config.py
+++ linux/.config/qutebrowser/config.py
@@ -1,4 +1,5 @@
-# Customizations done via the UI (:set, :bind and :unbind) are stored in the 
+# Customizations done via the UI (:set, :bind and :unbind) are stored in the
 # autoconfig.yml file so load those manually here.
 config.load_autoconfig()
 
+c.tabs.tabs_are_windows = False
